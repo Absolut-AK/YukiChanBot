5ff80aa9b8867a3ae6f5b2d207dd5563849eae6a
Absolut-AK <79558896+Absolut-AK@users.noreply.github.com>
2021-07-26 16:23:47 +0800

    Modifications

--- MainDiscordCommand.py
+++ MainDiscordCommand.py
@@ -1,6 +1,7 @@
 import discord
 from database import ids, profile
 from discord.ext import commands
+from DiscordBlackJack import blackJack
 
 client = discord.Client()
 client = commands.Bot(command_prefix='-')
@@ -15,5 +16,7 @@ async def p(ctx):
     username = ctx.message.author.id
     if ids(username):
         await ctx.send(username)
-        
+@client.command()
+async def bj(ctx):
+    blackJack()
 client.run('ODQ2NTg1MDMyMTg5NTQyNDEw.YKxpwA.zUSGN7_vFNV7xIVV69Ulss1AdoE')
--- database.py
+++ database.py
@@ -7,16 +7,12 @@ client = MongoClient('mongodb+srv://Yuki:A2d0a0m4@yukibot.7pgwj.mongodb.net/Yuki
 db = client["YukiBotDB"]
 collection = db["YukiBotDB"]
 
-# post = {"id": 0, "name": "abuno"}
-
-# collection.insert_one(post)
-
 #find id 
 #Account Things ↓
 def ids(post_id):
     accountTF = db.collection.find({"id": post_id})
     if accountTF == None:
-        collection.insert_One({"id": post_id})
+        db.collection.insert_One({"id": post_id})
     else:
         return True
 
@@ -24,4 +20,4 @@ def leaderBoardCollection():
     db.collection.find_many(sort=["coin", pymongo.DESCENDING, 5])
 
 def profile(id):
-    stats = Stats(collection.find({"__id": id}))
+    stats = Stats(collection.find({"id": id}))
